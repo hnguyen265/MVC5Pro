aad57091c7854f14860e2959f1d06427e0d4ca67
Nguyen Huu Hoang <hoangnguyen.csc@gmail.com>
2017-04-15 15:38:45 +0700

    First Changes

---
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SportStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repo;
+        // GET: Product
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public int pageSize = 4;
+        public ProductController(IProductRepository prod)
+        {
+            this.repo = prod;
+        }
+        public ViewResult List(int page =1)
+        {
+            return View(repo.Products
+                .OrderBy(p=>p.ProductID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                
+                );
+        }
+    }
+}
---
+++ SportStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using Ninject;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Concrete;
+using SportStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SportStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //{
+            //    new Product{Name = "Football", Price = 25},
+            //    new Product{Name = "Surf board", Price = 179},
+            //    new Product{Name = "Running shoes", Price = 95},
+
+            //});
+
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+    
+}
